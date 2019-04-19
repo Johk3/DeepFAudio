a198e596e56a7acaa7e755fb5eda57a615c0ad68
root <user3610@protonmail.com>
2019-04-19 14:34:20 +0000

    Model fully stable and tokenization is done

--- test.py
+++ test.py
@@ -10,29 +10,35 @@ np.random.seed(2016)
 
 # LSTM-autoencoder
 from LSTMAutoencoder import *
+print("\n\n\n\n")
 
 data = pickle.load(open("data/save.p", "rb"))
 
-data = tf.convert_to_tensor(data, dtype=tf.float32)
-print(data)
-
 # Constants
-batch_num = 128
+batch_num = 77
 hidden_num = 12
 step_num = 8
 elem_num = 1
 iteration = 10000
 
+#data = data[len(data)%step_num:]
+data = tf.convert_to_tensor(data, dtype=tf.float32)
+print(data.get_shape().as_list())
 # placeholder list
-p_input = tf.placeholder(tf.float32, shape=(batch_num, step_num, elem_num))
-p_inputs = [tf.squeeze(t, [1]) for t in tf.split(p_input, step_num, 1)]
-print(p_input)
-print("---")
+p_input = data
+
+# p_inputs = [tf.squeeze(t, [1]) for t in tf.split(p_input, step_num, 1)]
+p_inputs = []
+for t in tf.split(p_input, step_num, 1):
+    # Ignoring squeezing since it removes the necessary shape (77, ?)
+    p_inputs.append(t)
 print(p_inputs)
-exit(0)
 
 cell = tf.nn.rnn_cell.LSTMCell(hidden_num, use_peepholes=True)
-ae = LSTMAutoencoder(hidden_num, p_inputs, cell=cell, decode_without_input=True)
+try:
+    ae = LSTMAutoencoder(hidden_num, p_inputs, cell=cell, decode_without_input=True)
+except Exception as e:
+    print("WARNING ", e)
 
 saver = tf.train.Saver()
 
@@ -51,12 +57,12 @@ with tf.Session() as sess:
         d = np.linspace(0, step_num, step_num, endpoint=False).reshape([1, step_num, elem_num])
         d = np.tile(d, (batch_num, 1, 1))
         random_sequences = r + d
+        
+        random_sequences = random_sequences[:, :, 0]
 
+        
         (loss_val, _) = sess.run([ae.loss, ae.train], {p_input: random_sequences})
         if i % 1000 == 0:
-            print(len(d[0]))
-            print("---")
-            print(len(r[0]))
             print('iter %d:' % (i), loss_val)
         if i % 10000 == 0:
             save_path = saver.save(sess, "/tmp/model.ckpt")
